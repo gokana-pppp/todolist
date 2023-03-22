a90f2300725d0c7623a2486a08aa52521bb87f97
gokana <gokana-pppp@users.noreply.github.com>
2023-03-22 18:02:56 +0900

    cell4 ボタン　onclick追加、25行目から31行目追加

---
+++ todo1.ts
@@ -0,0 +1,31 @@
+const lists = <HTMLTableElement> document.getElementById("memoLists")!;
+const addButton:HTMLElement = document.getElementById("addBtn")!;
+
+const addMemo = ( ): void => {
+    const textMessage = <HTMLInputElement> document.getElementById("text");
+    //inputに入力された文字をnewMemoに定義する
+    const newMemo: string = textMessage.value;
+    
+    //table内に行を追加する
+    let row:any = lists.insertRow(-1);
+    //追加された行の中に4つのcellを作成する
+    let cell1:any = row.insertCell(-1);
+    cell1.id = "CELL1" //cell1はcssで管理するのでidをつけておく
+    let cell2:any = row.insertCell(-1);
+    let cell3:any = row.insertCell(-1);
+    let cell4:any = row.insertCell(-1);
+
+    cell2.innerHTML = newMemo;
+    cell3.innerHTML = '<button id="status">作業中</button>'  
+    cell4.innerHTML = '<button id="delete" onclick="deleteMemo(this)">削除</button>'
+    
+}
+
+addButton.addEventListener('click', addMemo);
+
+const deleteMemo =  (r: any): void => {
+    // 選択した行を削除する
+    let i = r.parentNode.parentNode.rowIndex;
+    lists.deleteRow(i);
+  
+}
